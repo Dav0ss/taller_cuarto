2b8ca6c11063057546ef303f5f31748720205686
dav0ss <dlscout92@gmail.com>
2023-10-26 20:25:32 -0300

    probando de nuevo

---
+++ hola.py
@@ -0,0 +1,41 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return 'hello word'
+
+@app.route('/tituloh2')
+def tituloh2():
+    return '<h2>Esto es un titulo HTML</h2>'
+
+@app.route('/parrafo')
+def parrafo():
+    return '<p>Esto es un parrafo</p>'
+
+@app.route('/get-lista')
+def lista():
+    return [
+        "administrador de sistemas",
+        "secretaria",
+        "gerente",
+        "vendedor",
+        "auxiliar de compra"
+    ]
+@app.route('/get-diccionario')
+def diccionario():
+    dicc1 = {
+        "apellidos" : "rojas",
+        "edad" : 64,
+        "fechanac" : "1960-06-01",
+        "id" : 36,
+        "nombres" : "Justo"
+    }
+    dicc2 = {
+        "apellidos" : "rojas",
+        "edad" : 64,
+        "fechanac" : "1960-06-01",
+        "id" : 36,
+        "nombres" : "Justo"
+    }
+    return [dicc1, dicc2]
